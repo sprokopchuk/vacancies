21274c72ec5edb8ec6a8a720410e7913eebdae91
Sergey Prokopchuk <svp@anahoret.com>
2016-01-28 16:44:04 +0200

    Scopes for opened and archived vacancies

--- app/models/vacancy.rb
+++ app/models/vacancy.rb
@@ -1,4 +1,8 @@
 class Vacancy < ActiveRecord::Base
   validates :title, :description, :deadline, presence: true
   belongs_to :company
+
+  scope :opened, -> {where("deadline > ?", Date.current)}
+  scope :archived, -> {where("deadline < ?", Date.current)}
+
 end

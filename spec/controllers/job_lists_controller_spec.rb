efaf7a514c2d1aefd7fcb0cbfc8af472f348e7a9
Sergey Prokopchuk <svp@anahoret.com>
2016-02-01 16:41:23 +0200

    Unit tests for JobListsController

---
+++ spec/controllers/job_lists_controller_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe JobListsController, type: :controller do
+  let(:authenticated_user) {FactoryGirl.create :user}
+  let(:vacancy) {FactoryGirl.build_stubbed :vacancy}
+
+  describe "GET #show" do
+    before do
+      allow(controller).to receive(:current_user).and_return authenticated_user
+    end
+    it "renders :show template" do
+      allow(request.env['warden']).to receive(:authenticate!).and_return(authenticated_user)
+      allow(authenticated_user).to receive(:vacancies).and_return [vacancy]
+      get :show
+      expect(response).to render_template :show
+    end
+
+    it "redirects to sign_in page if current_user is nil" do
+      get :show
+      expect(response).to redirect_to new_user_session_path
+    end
+
+  end
+
+end

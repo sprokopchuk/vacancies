70195dbbc94e7a683c9314412584ac73754e8c72
Sergey Prokopchuk <svp@anahoret.com>
2016-02-02 13:38:59 +0200

    Settigs for bootstrap

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,12 @@ module Vacancies
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g|
+      g.template_engine :haml
+  # you can also specify a different test framework or ORM here
+      g.test_framework  :rspec
+  # g.orm             :mongoid
+    end
   end
 end

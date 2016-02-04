c19263db45752a5e76b477c7acc439169bac47da
Sergey Prokopchuk <svp@anahoret.com>
2016-02-04 14:19:31 +0200

    Include in VacancySearch form for searching

--- app/queries/vacancy_search.rb
+++ app/queries/vacancy_search.rb
@@ -1,47 +1,68 @@
 class VacancySearch
-  def initialize(vacancies, params_search = {})
-    @vacancies, @params = vacancies, params_search
-  end
 
+  include ActiveModel::Model
+  include Virtus.model
+
+  attr_reader :country, :state, :city, :company_id
+  attribute :params, Hash
+  attribute :vacancies, ActiveRecord::Relation, :default => :default_vacancies
+  attribute :country, String, :default => :default_country
+  attribute :state, String, :default => :default_state
+  attribute :city, String, :default => :default_city
+  attribute :company_id, String, :default => :default_company_id
+  attribute :speciality_id, String, :default => :default_speciality_id
   def call
-    if @params[:search]
-      by_country
-      by_city
-      by_company
-    end
-    archived
+    p country
+    p city
+    by_country
+    by_city
+    by_company
     by_speciality
     @vacancies
   end
 
   private
 
-  def archived
-    if @params[:archived].present?
-      @vacancies = Vacancy.archived
-    end
+  def default_vacancies
+    Vacancy.all
+  end
+
+  def default_country
+    @params[:country]
+  end
+
+  def default_city
+    @params[:city]
+  end
+
+  def default_company_id
+    @params[:company_id]
+  end
+
+  def default_speciality_id
+    @params[:speciality_id]
   end
   def by_country
-    if @params[:search][:country].present?
-      @vacancies = @vacancies.where("country = ?", @params[:search][:country])
+    if country && !country.empty?
+      @vacancies = @vacancies.where("country = ?", country)
     end
   end
 
   def by_city
-    if @params[:search][:city].present?
-      @vacancies = @vacancies.where("city = ?", @params[:search][:city])
+    if city && !city.empty?
+      @vacancies = @vacancies.where("city = ?", city)
     end
   end
 
   def by_company
-    if @params[:search][:company_id].present?
-      @vacancies = @vacancies.where("company_id = ?", @params[:search][:company_id])
+    if company_id && !company_id.empty?
+      @vacancies = @vacancies.where("company_id = ?", company_id)
     end
   end
 
   def by_speciality
-    if @params[:speciality_id].present?
-      @vacancies = @vacancies.where("speciality_id = ?", @params[:speciality_id])
+    if speciality_id && !speciality_id.empty?
+      @vacancies = @vacancies.where("speciality_id = ?", speciality_id)
     end
   end
 end
--- spec/queries/vacancy_search_spec.rb
+++ spec/queries/vacancy_search_spec.rb
@@ -4,42 +4,48 @@ RSpec.describe VacancySearch, type: :model do
 
   let(:opened_vacancies) {FactoryGirl.create_list :vacancy, 3}
   let(:params) {{}}
-  subject{VacancySearch.new(Vacancy.all)}
   context "#call" do
     before do
       opened_vacancies
     end
 
     it "return the list of vacancies by default" do
-      expect(subject.call).to match_array(opened_vacancies)
+      expect(VacancySearch.new(params: {}).call).to match_array(opened_vacancies)
     end
 
     it "return the list of vacancies if params[:search][:country] is passed" do
       opened_vacancies[0].country = 'Uk'
-      params[:country] = 'Uk'
-      subject.instance_variable_set(:@params, params)
-      expect(subject.call).to match_array(Vacancy.where(:country => params[:country]))
+      opened_vacancies[0].save!
+      params[:country] = "Uk"
+      expect(
+        VacancySearch.new(params: params).call
+        ).to match_array(Vacancy.where(params))
     end
 
     it "return the list of vacancies if params[:search][:city] is passed" do
       opened_vacancies[0].city = 'Dnepr'
-      params[:city] = 'Dnepr'
-      subject.instance_variable_set(:@params, params)
-      expect(subject.call).to match_array(Vacancy.where(:city => params[:city]))
+      opened_vacancies[0].save!
+      params[:country] = "Dnepr"
+      expect(
+        VacancySearch.new(params: params).call
+        ).to match_array(Vacancy.where(:city => params[:city]))
     end
 
     it "return the list of vacancies if params[:search][:company_id] is passed" do
       opened_vacancies[0].company_id = 1
+      opened_vacancies[0].save!
       params[:company_id] = 1
-      subject.instance_variable_set(:@params, params)
-      expect(subject.call).to match_array(Vacancy.where(:company_id => params[:company_id]))
+      expect(
+        VacancySearch.new(params: params).call
+        ).to match_array(Vacancy.where(:company_id => params[:company_id]))
     end
 
     it "return the list of vacancies if params[:search][:speciality_id] is passed" do
       opened_vacancies[0].speciality_id = 1
       params[:speciality_id] = 1
-      subject.instance_variable_set(:@params, params)
-      expect(subject.call).to match_array(Vacancy.where(:speciality_id => params[:speciality_id]))
+      expect(
+        VacancySearch.new(params: params).call
+        ).to match_array(Vacancy.where(params))
     end
   end
 end

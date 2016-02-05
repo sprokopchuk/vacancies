7bd18d7acdbe4314addadf1d017f0bc27bdc40b7
Sergey Prokopchuk <svp@anahoret.com>
2016-02-05 17:12:15 +0200

    Add route for custom RegistrationsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root "vacancies#index"
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "registrations" }
   resources :vacancies, only: :index do
     get 'archived', on: :collection
   end
-  resource :company, shallow: true do
-    resources :vacancies, except: :index do
+  resource :company do
+    resources :build, controller: 'company/build', only: [:show, :update, :create]
+    resources :vacancies, except: :index, shallow: true do
       post 'attach_resume', on: :member
     end
   end

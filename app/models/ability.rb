f8286989ad0232e89228f5df1fcea5ca36d37b57
Sergey Prokopchuk <svp@anahoret.com>
2016-02-05 17:10:27 +0200

    Add managment user company

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,8 +4,9 @@ class Ability
   def initialize(user)
 
     user ||= User.new
-    if user.employer?
-      can :manage, Vacancy, :company => {:user_id => user.id}
+    if user.role? :employer
+      can :manage, Company, :user_id => user.id
+      can :manage, Vacancy, :company => {:user_id => user.id} if user.approved?
       can :read, Vacancy
     else
       can :attach_resume, Vacancy if user.persisted?

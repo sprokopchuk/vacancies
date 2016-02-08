ca94bd58d71dd4b1972fe400a073bc4543f82991
Sergey Prokopchuk <svp@anahoret.com>
2016-02-08 10:56:24 +0200

    fix some tests for VacancySearch

--- spec/queries/vacancy_search_spec.rb
+++ spec/queries/vacancy_search_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe VacancySearch, type: :model do
 
-  let(:opened_vacancies) {FactoryGirl.create_list :vacancy, 3}
+  let(:company) {FactoryGirl.create :company}
+  let(:opened_vacancies) {FactoryGirl.create_list :vacancy, 3, company: company}
   let(:params) {{}}
   context "#call" do
     before do
@@ -25,16 +26,14 @@ RSpec.describe VacancySearch, type: :model do
     it "return the list of vacancies if params[:search][:city] is passed" do
       opened_vacancies[0].city = 'Dnepr'
       opened_vacancies[0].save!
-      params[:country] = "Dnepr"
+      params[:city] = "Dnepr"
       expect(
         VacancySearch.new(params: params).call
         ).to match_array(Vacancy.where(:city => params[:city]))
     end
 
     it "return the list of vacancies if params[:search][:company_id] is passed" do
-      opened_vacancies[0].company_id = 1
-      opened_vacancies[0].save!
-      params[:company_id] = 1
+      params[:company_id] = company.id
       expect(
         VacancySearch.new(params: params).call
         ).to match_array(Vacancy.where(:company_id => params[:company_id]))

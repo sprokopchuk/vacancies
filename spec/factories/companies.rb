d2779b4cc73b86ef22dfc3a1b91715b975e95411
Sergey Prokopchuk <svp@anahoret.com>
2016-02-08 10:44:50 +0200

    Add unit tests for validations company

--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -2,6 +2,10 @@ FactoryGirl.define do
   factory :company do
     name {Faker::Company.name}
     description {Faker::Lorem.sentence}
+    city {Faker::Address.city}
+    country {Faker::Address.country_code}
+    state {Faker::Address.state_abbr}
+
     user
   end
 end
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -2,6 +2,53 @@ require 'rails_helper'
 
 RSpec.describe Company, type: :model do
   subject {FactoryGirl.create :company}
-  it {expect(subject).to validate_presence_of(:name)}
+  context "if active_or_info?" do
+    before do
+      allow(subject).to receive(:active_or_info?).and_return true
+    end
+    it {expect(subject).to validate_presence_of(:name)}
+    it {expect(subject).to validate_presence_of(:description)}
+  end
+
+  context "unless active_or_info?" do
+    before do
+      allow(subject).to receive(:active_or_info?).and_return false
+    end
+    it {expect(subject).not_to validate_presence_of(:name)}
+    it {expect(subject).not_to validate_presence_of(:description)}
+  end
+
+  context "if active_or_country?" do
+    before do
+      allow(subject).to receive(:active_or_country?).and_return true
+    end
+
+    it {expect(subject).to validate_presence_of(:country)}
+  end
+
+  context "unless active_or_country?" do
+    before do
+      allow(subject).to receive(:active_or_country?).and_return false
+    end
+
+    it {expect(subject).not_to validate_presence_of(:country)}
+  end
+
+  context "if active_or_city?" do
+    before do
+      allow(subject).to receive(:active_or_city?).and_return true
+    end
+
+    it {expect(subject).to validate_presence_of(:city)}
+  end
+
+  context "unless active_or_city?" do
+    before do
+      allow(subject).to receive(:active_or_city?).and_return false
+    end
+
+    it {expect(subject).not_to validate_presence_of(:city)}
+  end
   it {expect(subject).to belong_to(:user)}
+  it {expect(subject).to have_many(:vacancies)}
 end

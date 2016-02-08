ac5079d1fc7d3a2afb75e2b085bc518000b02001
Sergey Prokopchuk <svp@anahoret.com>
2016-02-08 17:21:08 +0200

    Update creation of models

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,18 +15,9 @@ if User.any? || Company.any? || Vacancy.any? || Speciality.any?
   Speciality.delete_all
   puts "All is gone"
 else
-  countries = CountryStateSelect.countries_collection
-  first_country = countries[54].last
-  second_country = countries[6].last
-  third_country = countries[15].last
-  states_for_first_country = CountryStateSelect.collect_states first_country
-  states_for_second_country = CountryStateSelect.collect_states second_country
-  states_for_third_country = CountryStateSelect.collect_states third_country
-  first_cities = CountryStateSelect.collect_cities(states_for_first_country[5].last, first_country)
-  second_cities = CountryStateSelect.collect_cities(states_for_second_country[4].last, second_country)
-  third_cities = CountryStateSelect.collect_cities(states_for_third_country[4].last, third_country)
-  user = FactoryGirl.create(:user, email: "admin@admin.com")
-  employer = FactoryGirl.create(:employer, email: "employer@admin.com")
+   FactoryGirl.create :admin, email: "admin@gmail.com", approved: true
+  user = FactoryGirl.create(:user, email: "applicant@gmail.com")
+  employer = FactoryGirl.create(:employer, email: "employer@gmail.com", approved: true)
   puts "Create users"
   com = FactoryGirl.create(:company, user: employer)
   puts "Create company"
@@ -35,41 +26,31 @@ else
   10.times.each do
     FactoryGirl.create :vacancy,
       company: com,
-      speciality: profs[0],
-      country: first_country,
-      city: first_cities[0]
+      speciality: profs[0]
   end
 
   10.times.each do
     FactoryGirl.create :vacancy,
     company: FactoryGirl.create(:company),
-    speciality: profs[1],
-    country: second_country,
-    city: second_cities[0]
+    speciality: profs[1]
   end
 
   10.times.each do
     FactoryGirl.create :vacancy,
     company: com,
-    speciality: profs[2],
-    country: third_country,
-    city: third_cities[0]
+    speciality: profs[2]
   end
 
   10.times.each do
     FactoryGirl.create :vacancy,
     company: FactoryGirl.create(:company),
-    speciality: profs[3],
-    country: second_country,
-    city: second_cities[0]
+    speciality: profs[3]
   end
 
   10.times.each do
     FactoryGirl.create :archived_vacancy,
     company: com,
-    speciality: profs[3],
-    country: first_country,
-    city: first_cities[0]
+    speciality: profs[3]
   end
 
   puts "Create vacancies"

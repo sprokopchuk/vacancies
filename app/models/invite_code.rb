080df45098c14d9027b121cb99f7e6a50de932de
Sergey Prokopchuk <svp@anahoret.com>
2016-02-12 17:12:41 +0200

    Add method #get_user_used_invite

--- app/models/invite_code.rb
+++ app/models/invite_code.rb
@@ -1,3 +1,7 @@
 class InviteCode < ActiveRecord::Base
   belongs_to :user
+
+  def get_user_used_invite
+    User.where(:invite_code => self.code).take
+  end
 end
--- spec/models/invite_code_spec.rb
+++ spec/models/invite_code_spec.rb
@@ -2,5 +2,15 @@ require 'rails_helper'
 
 RSpec.describe InviteCode, type: :model do
   subject{FactoryGirl.create :invite_code}
+  let(:manager) {FactoryGirl.create :manager, invite_code: subject.code}
   it {expect(subject).to belong_to(:user)}
+
+  context "#get_user_used_invite" do
+    before do
+      manager
+    end
+    it  "return a manager" do
+      expect(subject.get_user_used_invite).to eq manager
+    end
+  end
 end

344de6bf700b8dc9d0b6ffef57e4124ed386467e
Sergey Prokopchuk <svp@anahoret.com>
2016-02-15 14:44:16 +0200

    Add charts to rails_admin

--- app/models/vacancy.rb
+++ app/models/vacancy.rb
@@ -1,4 +1,5 @@
 class Vacancy < ActiveRecord::Base
+  include RailsAdminCharts
   validates :title, :description, :deadline, :city, :country, presence: true
   belongs_to :company
   belongs_to :speciality

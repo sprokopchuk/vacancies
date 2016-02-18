8ff224b5bf5c35ca7b9399e99b01d668d964085e
Sergey Prokopchuk <svp@anahoret.com>
2016-02-18 09:29:39 +0200

    Feature tests for searching vacancies

---
+++ spec/features/vacancy_search_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+feature 'Vacancy search' do
+
+  given(:company) {FactoryGirl.create :company, city: "Dnper", country: "UA"}
+  given(:vacancy) {FactoryGirl.create :vacancy, company: company}
+  given(:other_vacancy) {FactoryGirl.create :vacancy, company: company}
+  background do
+    other_vacancy
+    FactoryGirl.create_list :vacancy, 20
+    vacancy
+    visit root_path
+  end
+
+  scenario "see a other_vacancy but don't see vacancy" do
+    expect(page).not_to have_link vacancy.title.capitalize
+    expect(page).to have_link other_vacancy.title.capitalize
+  end
+
+  scenario 'by clicking link on name of country' do
+    click_link "", href: vacancies_path(:country => other_vacancy.country)
+    expect(page).to have_link vacancy.title.capitalize
+  end
+
+  scenario 'by clicking link on name of company' do
+    click_link other_vacancy.company.name
+    expect(page).to have_link vacancy.title.capitalize
+  end
+
+  scenario 'by clicking link on name of city' do
+    click_link other_vacancy.city
+    expect(page).to have_link vacancy.title.capitalize
+  end
+
+  scenario 'by fill in form of search' do
+    fill_in "search", with: other_vacancy.city
+    click_button "Search"
+    expect(page).to have_link vacancy.title.capitalize
+  end
+end

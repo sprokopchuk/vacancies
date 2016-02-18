f090c9d97752de2791a1dc0f7dfd5a91e9b13709
Sergey Prokopchuk <svp@anahoret.com>
2016-02-18 09:28:00 +0200

    Feature tests for employer

---
+++ spec/features/abilities_employer_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+feature 'Employer' do
+  given(:employer) {FactoryGirl.create :employer, approved: true}
+  given(:company) {FactoryGirl.create :company, user: employer}
+  given(:vacancy) {FactoryGirl.build :vacancy}
+  given(:user) {FactoryGirl.create :user}
+  given(:specialities) {FactoryGirl.create_list :speciality, 3}
+  background do
+    login_as employer, scope: :user
+    company
+    visit root_path
+  end
+
+  scenario 'post a vacancy' do
+    specialities
+    click_link "Post a vacancy"
+    within "#new_vacancy" do
+      fill_in "vacancy[title]", with: vacancy.title
+      fill_in "vacancy[deadline]", with: Date.current.to_date
+      select specialities[1].name, :from => "vacancy[speciality_id]"
+      fill_in "vacancy[description]", with: vacancy.description
+    end
+    click_button "Submit"
+    expect(page).to have_content('Vacancy was successfully created.')
+  end
+
+  scenario 'generate new invite code' do
+    click_link "Invite codes"
+    click_link "Generate an new invite code"
+    expect(page).to have_content(employer.invite_codes[0].code)
+  end
+
+  scenario 'see the list of vacancies of own company' do
+    vacancy.company_id = employer.company.id
+    vacancy.save
+    click_link "My company"
+    expect(page).to have_link vacancy.title.capitalize
+  end
+
+  scenario 'close a vacancy' do
+    vacancy.company_id = employer.company.id
+    vacancy.save
+    click_link "My company"
+    click_link "", :href => close_vacancy_path(vacancy, :company_id => company.id)
+    expect(page).to have_content("Vacancy is closed")
+  end
+
+  scenario 'see applied resume in vacancy' do
+    vacancy.company_id = employer.company.id
+    vacancy.save
+    vacancy.attach_resume user, File.new(File.join(Rails.root, 'spec', 'support', 'logo_image.png'))
+    click_link "My company"
+    click_link vacancy.title.capitalize
+    expect(page).to have_content user.full_name
+  end
+
+  scenario 'see own profile' do
+    click_link "My profile"
+    expect(page).to have_content(employer.full_name)
+  end
+end

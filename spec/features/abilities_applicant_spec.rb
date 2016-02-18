73f94d8616439f5a5b01f55445ddb9d64320adbd
Sergey Prokopchuk <svp@anahoret.com>
2016-02-18 09:27:29 +0200

    Feature tests for applicant

---
+++ spec/features/abilities_applicant_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+feature 'Applicant' do
+  given(:user) {FactoryGirl.create :user}
+  given(:vacancy) {FactoryGirl.create :vacancy}
+  background do
+    login_as user, scope: :user
+    vacancy
+    visit root_path
+  end
+  scenario 'attach own resume to the job' do
+    click_link "#{vacancy.title.capitalize}"
+    attach_file "vacancy[file]", user.resume.file.path
+    click_button "Send resume"
+    expect(page).to have_content('Your resumne was successfully sent.')
+  end
+
+  scenario 'see applied jobs' do
+    vacancy.attach_resume user, File.new(File.join(Rails.root, 'spec', 'support', 'logo_image.png'))
+    click_link "Job List"
+    expect(page).to have_link vacancy.title.capitalize
+  end
+
+  scenario 'see own profile' do
+    click_link "My profile"
+    expect(page).to have_content(user.full_name)
+  end
+end

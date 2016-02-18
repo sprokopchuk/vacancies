6d215492456ba930d14cf64e3912bffb1492f8c3
Sergey Prokopchuk <svp@anahoret.com>
2016-02-18 09:23:35 +0200

    Some refactoring CompaniesController

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,13 +1,7 @@
 class CompaniesController < ApplicationController
 
   load_and_authorize_resource
-  before_action :vacancies
   def show
-  end
-
-  private
-
-  def vacancies
     @vacancies = @company.vacancies.page(params[:page])
   end
 
